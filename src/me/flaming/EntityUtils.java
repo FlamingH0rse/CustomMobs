e8c49ab5f37d9a66ca50b1d277dc201c8c00e1df
flightofnoobs <42741758+flightofnoobs@users.noreply.github.com>
2023-12-26 16:15:26 +0800

    Code cleanup

--- src/me/flaming/EntityUtils.java
+++ src/me/flaming/EntityUtils.java
@@ -5,6 +5,7 @@ import me.flaming.classes.CustomMobItem;
 import me.flaming.classes.EntityInventory;
 import org.bukkit.*;
 import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.entity.Ageable;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
@@ -30,6 +31,10 @@ public class EntityUtils {
             return colorStr("&cThe specified mob does not exist in the current context");
         }
 
+        if (location.getWorld() == null) {
+            return colorStr("&cCurrent location is not in a valid world");
+        }
+
         Entity entity = location.getWorld().spawnEntity(location, mob.getType());
         // I don't think this will ever happen as it will be handled internally anyways
         if (!(entity instanceof LivingEntity living)) {
@@ -38,92 +43,19 @@ public class EntityUtils {
         }
         NamespacedKey key = new NamespacedKey(getPlugin(), "JDnD-weDJ-KDe-DSaw");
 
-        // General Information about the mob
+        // Set general Information about the mob
         living.getPersistentDataContainer().set(key, PersistentDataType.STRING, mobInternalName);
         living.setCustomName(colorStr(mob.getDisplayName()));
 
-        // Stats
-        living.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(mob.getHealth());
-        living.setHealth(mob.getHealth());
-        living.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).setBaseValue(mob.getDamage());
-        living.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(mob.getSpeed());
+        // set Entity attributes
+        setEntityAttributes(mob, living);
 
-        if (mob.getInv() != null) {
-            // lol
-            UnsafeValues unsafe = Bukkit.getUnsafe();
-            EntityInventory mobInv = mob.getInv();
-            // Currently the code is very repetitive
-
-            CustomMobItem mainHandItem = mobInv.getMainHand();
-            CustomMobItem leftHandItem = mobInv.getLeftHand();
-            CustomMobItem headItem = mobInv.getHead();
-            CustomMobItem bodyItem = mobInv.getBody();
-            CustomMobItem legsItem = mobInv.getLegs();
-            CustomMobItem footItem = mobInv.getFoot();
-
-            EntityEquipment entityEquipment = living.getEquipment();
-
-            if (entityEquipment != null) {
-                ItemStack mainHand = null;
-                ItemStack leftHand = null;
-                ItemStack head;
-                ItemStack body;
-                ItemStack legs;
-                ItemStack foot;
-
-                if (mainHandItem != null && mainHandItem.getItem() != null) {
-                    mainHand = new ItemStack(mainHandItem.getItem().getKey());
-                    mainHand = unsafe.modifyItemStack(mainHand, mainHandItem.getItem().getValue());
-                }
-
-                if (leftHandItem != null && leftHandItem.getItem() != null) {
-                    leftHand = new ItemStack(leftHandItem.getItem().getKey());
-                    leftHand = unsafe.modifyItemStack(leftHand, leftHandItem.getItem().getValue());
-                }
-
-                if (headItem != null && headItem.getItem() != null) {
-                    head = new ItemStack(headItem.getItem().getKey());
-                    head = unsafe.modifyItemStack(head, headItem.getItem().getValue());
-                    entityEquipment.setHelmet(head, false);
-                    entityEquipment.setHelmetDropChance(0);
-                }
-
-                if (bodyItem != null && bodyItem.getItem() != null) {
-                    body = new ItemStack(bodyItem.getItem().getKey());
-                    body = unsafe.modifyItemStack(body, bodyItem.getItem().getValue());
-                    entityEquipment.setChestplate(body, false);
-                    entityEquipment.setChestplateDropChance(0);
-                }
-
-                if (legsItem != null && legsItem.getItem() != null) {
-                    legs = new ItemStack(legsItem.getItem().getKey());
-                    legs = unsafe.modifyItemStack(legs, legsItem.getItem().getValue());
-                    entityEquipment.setLeggings(legs, false);
-                    entityEquipment.setLeggingsDropChance(0);
-                }
-
-                if (footItem != null && footItem.getItem() != null) {
-                    foot = new ItemStack(footItem.getItem().getKey());
-                    foot = unsafe.modifyItemStack(foot, footItem.getItem().getValue());
-                    entityEquipment.setBoots(foot, false);
-                    entityEquipment.setBootsDropChance(0);
-                }
-
-                entityEquipment.setItemInMainHand(mainHand);
-                entityEquipment.setItemInOffHand(leftHand);
-            }
-
-            // Extra properties (may be added in config.yml) alongside with defaultAI (like zombies chasing villagers)
-            if (living instanceof Ageable) {
-                ((Ageable) living).setAdult();
-            }
+        // Set extra properties (may be added in config.yml) alongside with defaultAI (like zombies chasing villagers)
+        if (living instanceof Ageable) {
+            ((Ageable) living).setAdult();
         }
 
         return colorStr("&aSuccessfully spawned the mob!");
-    }
-
-    private void setEntityProperties() {
-
     }
 
     // This will also return null if the entity provided is not a valid mob
@@ -145,7 +77,95 @@ public class EntityUtils {
                 entity.remove();
             }
         }
-
         return entity;
+    }
+
+    private void setEntityAttributes(@NotNull CustomEntity mob, @NotNull LivingEntity living) {
+        AttributeInstance healthAttribute = living.getAttribute(Attribute.GENERIC_MAX_HEALTH);
+        AttributeInstance attackDamageAttribute = living.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE);
+        AttributeInstance movementSpeedAttribute = living.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED);
+
+        // Stats (hope its pass by reference)
+        if (healthAttribute != null) {
+            healthAttribute.setBaseValue(mob.getHealth());
+            living.setHealth(mob.getHealth());
+        }
+
+        if (attackDamageAttribute != null) {
+            attackDamageAttribute.setBaseValue(mob.getDamage());
+        }
+
+        if (movementSpeedAttribute != null) {
+            movementSpeedAttribute.setBaseValue(mob.getSpeed());
+        }
+
+        if (mob.getInv() != null) {
+            setEntityEquipment(mob, living);
+        }
+    }
+
+    private void setEntityEquipment(@NotNull CustomEntity mob, @NotNull LivingEntity living) {
+        UnsafeValues unsafe = Bukkit.getUnsafe();
+        EntityInventory mobInv = mob.getInv();
+
+        // Currently the code is very repetitive
+        CustomMobItem mainHandItem = mobInv.getMainHand();
+        CustomMobItem leftHandItem = mobInv.getLeftHand();
+        CustomMobItem headItem = mobInv.getHead();
+        CustomMobItem bodyItem = mobInv.getBody();
+        CustomMobItem legsItem = mobInv.getLegs();
+        CustomMobItem footItem = mobInv.getFoot();
+
+        EntityEquipment entityEquipment = living.getEquipment();
+
+        if (entityEquipment != null) {
+            ItemStack mainHand = null;
+            ItemStack leftHand = null;
+            ItemStack head;
+            ItemStack body;
+            ItemStack legs;
+            ItemStack foot;
+
+            if (mainHandItem != null && mainHandItem.getItem() != null) {
+                mainHand = new ItemStack(mainHandItem.getItem().getKey());
+                mainHand = unsafe.modifyItemStack(mainHand, mainHandItem.getItem().getValue());
+            }
+
+            if (leftHandItem != null && leftHandItem.getItem() != null) {
+                leftHand = new ItemStack(leftHandItem.getItem().getKey());
+                leftHand = unsafe.modifyItemStack(leftHand, leftHandItem.getItem().getValue());
+            }
+
+            if (headItem != null && headItem.getItem() != null) {
+                head = new ItemStack(headItem.getItem().getKey());
+                head = unsafe.modifyItemStack(head, headItem.getItem().getValue());
+                entityEquipment.setHelmet(head, false);
+                entityEquipment.setHelmetDropChance(0);
+            }
+
+            if (bodyItem != null && bodyItem.getItem() != null) {
+                body = new ItemStack(bodyItem.getItem().getKey());
+                body = unsafe.modifyItemStack(body, bodyItem.getItem().getValue());
+                entityEquipment.setChestplate(body, false);
+                entityEquipment.setChestplateDropChance(0);
+            }
+
+            if (legsItem != null && legsItem.getItem() != null) {
+                legs = new ItemStack(legsItem.getItem().getKey());
+                legs = unsafe.modifyItemStack(legs, legsItem.getItem().getValue());
+                entityEquipment.setLeggings(legs, false);
+                entityEquipment.setLeggingsDropChance(0);
+            }
+
+            if (footItem != null && footItem.getItem() != null) {
+                foot = new ItemStack(footItem.getItem().getKey());
+                foot = unsafe.modifyItemStack(foot, footItem.getItem().getValue());
+                entityEquipment.setBoots(foot, false);
+                entityEquipment.setBootsDropChance(0);
+            }
+
+            entityEquipment.setItemInMainHand(mainHand);
+            entityEquipment.setItemInOffHand(leftHand);
+        }
     }
 }
